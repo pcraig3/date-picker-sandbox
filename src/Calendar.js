5f652bd306a642706da35fac93f5a6687471b4fa
Paul Craig <paul_craig_16@hotmail.com>
2018-05-01 22:35:44 -0400

    Add internal state back to calendar

--- src/Calendar.js
+++ src/Calendar.js
@@ -6,13 +6,17 @@ export default class Calendar extends Component {
   constructor(props) {
     super(props);
     this.handleDayClick = this.handleDayClick.bind(this);
+    this.state = {
+      selectedDays: this.props.input.value || []
+    };
+    this.props.input.value = this.state.selectedDays;
   }
   handleDayClick(day, { selected, disabled }) {
     if (disabled) {
       return;
     }
 
-    const selectedDays = this.props.input.value || [];
+    const { selectedDays } = this.state;
     if (selected) {
       const selectedIndex = selectedDays.findIndex(selectedDay =>
         DateUtils.isSameDay(selectedDay, day)
@@ -22,7 +26,8 @@ export default class Calendar extends Component {
       selectedDays.push(day);
     }
 
-    this.props.input.value = selectedDays;
+    this.setState({ selectedDays });
+    this.props.input.value = this.state.selectedDays;
     this.props.input.onChange(this.props.input.value);
   }
 
@@ -35,12 +40,13 @@ export default class Calendar extends Component {
           month={new Date(2018, 5)}
           numberOfMonths={1}
           disabledDays={[{ daysOfWeek: [0, 6] }]}
+          selectedDays={value || []}
           onDayClick={this.handleDayClick}
           onFocus={() => onFocus(value)}
           onBlur={() => onBlur(value)}
         />
         <p>selected days:</p>
-        <pre>{JSON.stringify(this.props.input.value)}</pre>
+        <pre>{JSON.stringify(value)}</pre>
       </div>
     );
   }
